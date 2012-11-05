2064dd53ea7c9c7cd306d51760bc4ca7772c68bf
Jakob Wanner <deadzac@gmx.net>
2012-11-05 17:27:21 +0100

    Added validations to Users (Chapter 6.2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, presence: true, length: { maximum: 50, minimum: 5 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
+  before_save { |user| user.email = email.downcase }                    
+  
 end
